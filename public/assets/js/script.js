725466ebc686e00195494c553c16d51570fcd6d9
Steve Diabo <stevediabo@gmail.com>
2020-10-07 19:40:59 -0400

    roughing out content

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,25 @@
 (function ($) {
-  "use strict"; // Start of use strict
+	"use strict"; // Start of use strict
 
-  $(document).ready(function () {
+	$(document).ready(function () {
 
-  });
+		setVideoContainer();
+
+	});
+
+	function setVideoContainer() {
+		$('.video-container .video-poster').on('click', function() {
+
+			var container = $(this).parent();
+			var frame = container.find('.video-frame');
+			var poster = container.find('.video-poster');
+
+			var videoID = 'KnX1YlXxcvY';
+
+			container.addClass('video-open');
+			frame.prepend('<iframe src="https://www.youtube.com/embed/' + videoID + '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+
+		})
+	}
+
 })(jQuery); // End of use strict
