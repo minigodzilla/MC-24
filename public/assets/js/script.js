2cdec60a7c04227e96d9cf148a108487a794f547
Steve Diabo <stevediabo@gmail.com>
2020-10-14 15:08:58 -0400

    vimeo

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -29,10 +29,8 @@
 			var frame = container.find('.ec-video-frame');
 			var poster = container.find('.ec-video-poster');
 
-			var videoID = 'KnX1YlXxcvY';
-
 			container.addClass('ec-video-open');
-			frame.prepend('<iframe src="https://www.youtube.com/embed/' + videoID + '?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+			frame.prepend('<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/468269570?autoplay=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>');
 
 		})
 	}
